4d48a9c648d513890d6b73d5914997816a9bab6c
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2023-07-20 01:12:55 -0300

    updated user to _id

--- src/controllers/Panel/VoteParlamentaries/Presence.ts
+++ src/controllers/Panel/VoteParlamentaries/Presence.ts
@@ -4,12 +4,11 @@ import VoteModel from "../../../models/VoteModel"
 export const Presence = async (req, res) => {
   const { user } = req.params 
   const { presence } = req.body
-  console.log(user, presence, "user e presence")
 
   if(presence == null || presence == undefined){
     res.status(404).json({message: "Presença não foi cadastrada!"})
   }
-  const response = await VoteModel.findOneAndUpdate({user:user}, {
+  const response = await VoteModel.findOneAndUpdate({_id:user}, {
     presenca: presence
   })
 
