b776d9ea7655aee6f996aa532fb6f8bbcc309bee
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2023-08-19 16:54:23 -0300

    updated at

--- dist/src/controllers/PublicRoutes/ParlametariesList/index.js
+++ dist/src/controllers/PublicRoutes/ParlametariesList/index.js
@@ -9,15 +9,22 @@ const url = process.env.URL_INTERLEGIS;
 const ParlamentariesList = async (req, res) => {
     const parlamentares = await axios_1.default.get(`${url}/api/parlamentares/parlamentar/?page_size=100`);
     const filiacao = await axios_1.default.get(`${url}/api/parlamentares/partido/?page_size=100`);
+    console.log(parlamentares.data.results, "parlamentares");
+    // console.log(filiacao.data.results, "filiacao")
     let response = [];
+    let obj;
     for (let parl of parlamentares.data.results) {
         for (let fil of filiacao.data.results) {
             if (parl.id == fil.id && parl.ativo) {
-                let obj = {
+                obj = {
                     id: parl.id,
                     __str__: parl.__str__,
                     fotografia: parl.fotografia,
                     sigla: fil.sigla,
+                    data_nascimento: parl.data_nascimento,
+                    nome_completo: parl.nome_completo,
+                    ativo: parl.ativo,
+                    email: parl.email,
                     nome: fil.nome,
                     logo_partido: fil.logo_partido
                 };
--- src/controllers/PublicRoutes/ParlametariesList/index.ts
+++ src/controllers/PublicRoutes/ParlametariesList/index.ts
@@ -5,21 +5,28 @@ const url = process.env.URL_INTERLEGIS
 export const ParlamentariesList = async (req, res) => {
   const parlamentares = await axios.get(`${url}/api/parlamentares/parlamentar/?page_size=100`)
   const filiacao = await axios.get(`${url}/api/parlamentares/partido/?page_size=100`)
+  console.log(parlamentares.data.results, "parlamentares")
+  // console.log(filiacao.data.results, "filiacao")
 
   let response = []
+  let obj;
   for(let parl of parlamentares.data.results){
     for(let fil of filiacao.data.results){
       if( parl.id == fil.id && parl.ativo){
-        let obj = {
-          id: parl.id,
-          __str__: parl.__str__,
-          fotografia: parl.fotografia,
-          sigla: fil.sigla,
-          nome: fil.nome,
+          obj = {
+          id: parl.id ,
+          __str__: parl.__str__ ,
+          fotografia: parl.fotografia ,
+          sigla: fil.sigla ,
+          data_nascimento: parl.data_nascimento ,
+          nome_completo: parl.nome_completo,
+          ativo: parl.ativo,
+          email: parl.email,
+          nome: fil.nome ,
           logo_partido: fil.logo_partido
         }
         response.push(obj)
-      }
+      } 
     }
   }
 
