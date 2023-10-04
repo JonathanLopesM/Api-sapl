f3fb154bc6226de8626a86d6c577c4fa6acc6338
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2023-10-04 02:17:10 +0000

    correção em rotas e limitaçÃão por página de matérias

--- src/controllers/PublicRoutes/Legislation/index.ts
+++ src/controllers/PublicRoutes/Legislation/index.ts
@@ -3,7 +3,7 @@ const url = process.env.URL_INTERLEGIS;
 
 export const Legislation = async ( req, res ) => {
   const { page, year, type, ementa, number } = req.query
-  const getUrl = `${url}/api/norma/normajuridica/?o=-data_apresentacao&page=${page}&ano=${year ? year : ''}&tipo=${type ? type : ''}&ementa__icontains=${ementa ? ementa : ''}&numero=${number? number : ''}&page_size=5`
+  const getUrl = `${url}/api/norma/normajuridica/?o=-data&page=${page}&ano=${year ? year : ''}&tipo=${type ? type : ''}&ementa__icontains=${ementa ? ementa : ''}&numero=${number? number : ''}&page_size=5`
 
   const respo = await axios.get(getUrl)
   const response = respo.data
--- src/controllers/PublicRoutes/Matters/index.ts
+++ src/controllers/PublicRoutes/Matters/index.ts
@@ -5,7 +5,7 @@ export const MattersLegis = async (req, res) => {
   try {
     const { page, year, type, ementa, number } = req.query
     
-    const getUrl = `${url}/api/materia/materialegislativa/?o=-data_apresentacao&page=${page}&ano=${year ? year : ''}&tipo=${type ? type : ''}&ementa__icontains=${ementa ? ementa : ''}&numero=${number ? number : ''}`
+    const getUrl = `${url}/api/materia/materialegislativa/?o=-data_apresentacao&page=${page}&ano=${year ? year : ''}&tipo=${type ? type : ''}&ementa__icontains=${ementa ? ementa : ''}&numero=${number ? number : ''}&page_size=5`
     const materiasResponse = await axios.get(getUrl);
     const pagination = materiasResponse.data.pagination
     const materias = materiasResponse.data.results;
